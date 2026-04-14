c17c41597a6ae9d8eef0b499b3678120e445e6f0
Vyshnavi Pola <123942712+vysh7@users.noreply.github.com>
2026-04-14 10:49:03 +0530

    schema.sql

--- Online Retail Sales Analysis /schema.sql
+++ Online Retail Sales Analysis /schema.sql
@@ -1,27 +1,40 @@
+DROP TABLE IF EXISTS Order_Items;
+DROP TABLE IF EXISTS Orders;
+DROP TABLE IF EXISTS Products;
+DROP TABLE IF EXISTS Categories;
+DROP TABLE IF EXISTS Customers;
+
 CREATE TABLE Customers (
-    customer_id NUMBER PRIMARY KEY,
-    name VARCHAR2(50),
-    city VARCHAR2(50)
+    customer_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    city VARCHAR(50)
+);
+
+CREATE TABLE Categories (
+    category_id INT PRIMARY KEY,
+    category_name VARCHAR(50)
 );
 
 CREATE TABLE Products (
-    product_id NUMBER PRIMARY KEY,
-    name VARCHAR2(50),
-    category VARCHAR2(50),
-    price NUMBER
+    product_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    category_id INT,
+    price INT,
+    FOREIGN KEY (category_id) REFERENCES Categories(category_id)
 );
 
 CREATE TABLE Orders (
-    order_id NUMBER PRIMARY KEY,
-    customer_id NUMBER,
+    order_id INT PRIMARY KEY,
+    customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Items (
-    order_id NUMBER,
-    product_id NUMBER,
-    quantity NUMBER,
+    order_id INT,
+    product_id INT,
+    quantity INT,
+    PRIMARY KEY (order_id, product_id),
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (product_id) REFERENCES Products(product_id)
 );
